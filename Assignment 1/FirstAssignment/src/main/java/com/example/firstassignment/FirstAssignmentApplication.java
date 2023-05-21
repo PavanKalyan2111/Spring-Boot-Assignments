70902c01219c90e1ffed7778e7d02d09d38898ec
Mandala Pavan Kalyan <55622145+PavanKalyan2111@users.noreply.github.com>
2023-05-21 17:46:37 +0530

    Add files via upload

---
+++ Assignment 1/FirstAssignment/src/main/java/com/example/firstassignment/FirstAssignmentApplication.java
@@ -0,0 +1,36 @@
+package com.example.firstassignment;
+
+import java.util.Arrays;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FirstAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAssignmentApplication.class, args);
+	}
+	
+//	@Bean
+//	CommandLineRunner getBeans(ApplicationContext appContext) {
+//		return args ->{
+//			
+//			String[] beans = ((ListableBeanFactory) appContext).getBeanDefinitionNames();
+//				
+//			System.out.println("Beans From Spring Boot : ");
+//			
+//			Arrays.sort(beans);
+//		
+//		for(String bean : beans) {
+//			System.out.println(bean);
+//			}
+//		};
+//	}
+		
+
+}
---
+++ Assignment 3/ThirdAssignment/src/main/java/com/example/thirdassignment/BankController.java
@@ -0,0 +1,21 @@
+package com.example.thirdassignment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BankController {
+		
+		@RequestMapping("/bank")
+		public String bankBranches() {
+			
+			return "bank";
+		}
+		
+		@RequestMapping("/bankservices")
+		public String bankServices() {
+			
+			return "bankservices";
+		}
+
+	}
